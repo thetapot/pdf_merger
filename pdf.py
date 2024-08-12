da9e92564993a7a98a498fb5f7c4fa9871edde3d
Zhihao Bai <rero@Zhihaos-MacBook-Pro.local>
2024-08-12 16:23:14 -0700

    modify python file name

---
+++ pdf.py
@@ -0,0 +1,22 @@
+import PyPDF2
+import sys
+
+
+def pdf_combiner(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        merger.append(pdf)
+        print(pdf)
+    merger.write('super.pdf')
+
+inputs = sys.argv[1:]
+pdf_combiner(inputs)
+
+# with open('dummy.pdf', 'rb') as file:
+#     reader = PyPDF2.PdfFileReader(file)
+#     page = reader.getPage(0)
+#     page.rotateCounterClockwise(90)
+#     writer = PyPDF2.PdfFileWriter()
+#     writer.addPage(page)
+#     with open('tilt.pdf', 'wb') as new_file:
+#         writer.write(new_file)
